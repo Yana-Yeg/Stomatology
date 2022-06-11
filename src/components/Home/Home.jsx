5dc99b692c991920a755c1d30392011b5ce66bfe
Yana Yegorina <89519566+Yana-Yeg@users.noreply.github.com>
2022-06-11 19:38:58 +0200

    image+logo

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,22 @@
 import style from "./Home.module.css";
+import tooth from "../../images/icon-tooth.png";
 
 const Home = () => {
   return (
     <div className={style.field}>
-      <h1 className={style.title}>Home</h1>
+      {/* <h1 className={style.title}>Home</h1> */}
+      <div className={style.flex}>
+        <img src={tooth} alt="" className={style.tooth} />
+        <p className={style.text_logo}>Your dantist</p>
+      </div>
+      <div className={style.wrapper}>
+        <p className={style.text}>
+          <span className={style.logo}>Your dantist</span> is a modern dental
+          office in the city center. We invite children, adults as well as the
+          elderly. An experienced dental team is waiting for you at <br />
+          al. Architects 10B
+        </p>
+      </div>
     </div>
   );
 };
