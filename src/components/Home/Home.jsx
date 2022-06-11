8f487f4e6e35e00c9cfaa8e3740f8fbe07010f85
Yana Yegorina <89519566+Yana-Yeg@users.noreply.github.com>
2022-06-11 14:45:16 +0200

    home

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import style from "./Home.module.css";
+
+const Home = () => {
+  return (
+    <div className={style.field}>
+      <h1 className={style.title}>Home</h1>
+    </div>
+  );
+};
+
+export default Home;
