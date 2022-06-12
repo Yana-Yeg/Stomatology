998c3e8b88408a19c6d131252bf0f4f58d2d6377
Yana Yegorina <89519566+Yana-Yeg@users.noreply.github.com>
2022-06-12 18:31:51 +0200

    style

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,34 +1,14 @@
-import { Link, Outlet } from "react-router-dom";
 import Contacts from "../components/--about/Contacts/Contacts";
 import Form from "../components/--about/Form/Form";
 
 const AboutPage = () => {
   return (
-    <>
-      <h1 style={{ textAlign: "center" }}>About us</h1>
-      {/* <ul
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          listStyleType: "none",
-        }}
-      >
-        <li style={{ marginLeft: "-20px" }}>
-          <Link to="contacts">Our contacts</Link>
-        </li>
-        <li style={{ marginLeft: "20px" }}>
-          <Link to="form">Form</Link>
-        </li>
-        <li style={{ marginLeft: "20px" }}>
-          <Link to="team">Our team</Link>
-        </li>
-      </ul> */}
-      {/* <Outlet /> */}
-      <div>
+    <div className="container">
+      <div className="aboutpage_wrapper">
         <Form />
         <Contacts />
       </div>
-    </>
+    </div>
   );
 };
 
