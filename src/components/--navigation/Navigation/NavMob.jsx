2ab67997cfc9ad5265bca15f2990caa405ce84a1
Yana Yegorina <89519566+Yana-Yeg@users.noreply.github.com>
2022-06-14 14:53:57 +0200

    mob menu

---
+++ src/components/--navigation/Navigation/NavMob.jsx
@@ -0,0 +1,31 @@
+import CustomeLink from "../CustomeLink/CustomeLink";
+import Icons from "../../../images/sprite.svg";
+import style from "./NavMob.module.scss";
+
+const NavMob = ({ isOpenModal, handleClose }) => {
+  return (
+    <div className={style.wrap}>
+      <button type="button" onClick={handleClose} className={style.btn_close}>
+        <svg className={style.icon_close} width="14" height="14">
+          <use xlinkHref={`${Icons}#icon-close`} />
+        </svg>
+      </button>
+
+      <div className={style.link_menu}>
+        <ul>
+          <li>
+            <CustomeLink to="/">Home</CustomeLink>
+          </li>
+          <li>
+            <CustomeLink to="/posts">Posts</CustomeLink>
+          </li>
+          <li>
+            <CustomeLink to="/about">About</CustomeLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default NavMob;
