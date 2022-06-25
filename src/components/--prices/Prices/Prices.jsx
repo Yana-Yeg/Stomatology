e9d9d859d9b495fc6cc7d07ffe58de2927ea58c4
Yana Yegorina <89519566+Yana-Yeg@users.noreply.github.com>
2022-06-25 22:08:23 +0200

    new page

--- src/components/--prices/Prices/Prices.jsx
+++ src/components/--prices/Prices/Prices.jsx
@@ -1,36 +1,32 @@
+import { useState } from "react";
 import Big_tooth from "../../../images/big-tooth.jpeg";
+import GeneralDentistry from "./offer item/GeneralDentistry";
+import Prevention from "./offer item/Prevention";
 import style from "./Prices.module.scss";
 
 const Prices = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsOpen((prev) => !prev);
+  };
+
   return (
-    <div>
+    <div className={style.wrap}>
       <div>
         <ul>
           <li>
-            <p>General dentistry</p>
-            <ul className={style.list}>
-              <li>filling of light hardening - PLN 220-320</li>
-              <li>
-                large-scale reconstruction (after, but with a canal) - 270-370
-                zlotys
-              </li>
-              <li>reconstruction with the use of fiberglass - 370 zlotys</li>
-              <li>direct composite veneer - 350 zlotys</li>
-              <li>therapeutic bandage - 160 zlotys</li>
-              <li>overhead bleaching - 1000 zlotys</li>
-              <li>
-                whitening of dead teeth: the first visa - 180 zlotys, the next -
-                120 zlotys
-              </li>
-            </ul>
-            <p>
-              anesthesia is included in the cost of procedures "These prices are
-              indicative, optional and are not an offer within the meaning of
-              the first part of Article 66 of the Civil Code. The final cost is
-              determined during a medical consultation and an individual
-              treatment plan.
-            </p>
+            <button className={style.btn} onClick={() => toggleModal()}>
+              General dentistry
+            </button>
+            {isOpen && <GeneralDentistry style={style} />}
           </li>
+          {/* <li>
+            <button className={style.btn} onClick={() => toggleModal()}>
+              Prevention
+            </button>
+            {isOpen && <Prevention style={style} />}
+          </li> */}
         </ul>
       </div>
       <img src={Big_tooth} alt="" />
