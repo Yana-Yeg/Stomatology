4e6566847486f2c9354d6c47f926a03da8cd2ce0
Yana Yegorina <89519566+Yana-Yeg@users.noreply.github.com>
2022-07-06 09:58:39 +0200

    fix: clear unuseble code

--- src/redux/counterSelector.js
+++
@@ -1,3 +0,0 @@
-// export const getCounterResult = (state) => state.counter.counterResult;
-// export const getCountRes = (state) => state.counter.countRes;
-// export const getIsLoading = (state) => state.counter.isLoading;
