75c68d787ee676a990152114d8f4220299309472
Yana Yegorina <89519566+Yana-Yeg@users.noreply.github.com>
2022-07-07 20:04:01 +0200

    fix: clear code

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import "./index.scss";
 import App from "./App";
-import store from "./redux/store";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
